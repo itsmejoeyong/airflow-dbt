814c755c295a4d70d22b48ac268bb5e590505501
JoeYong01 <joeanselmyz@gmail.com>
2024-04-25 06:49:21 +0800

    restructure repo to only include shared_src contents

--- dags/dbt/jaffle_shop_2/models/staging/stripe/stg_payments.sql
+++
@@ -1,9 +0,0 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    -- amount is stored in cents, convert it to dollars
-    created as created_at,
-    amount / 100 as amount
-from {{ source('stripe', 'payment') }}
